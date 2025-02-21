2e542ed57b3b32f929a934e88b01c2947dcd9f9c
fmacedosantos <felipemacedo986@gmail.com>
2025-02-21 09:10:04 -0300

    configuring typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './models/user.model';
+import { Message } from './models/message.model';
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }, 
+      entities: [User, Message],
+      synchronize: true, 
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
