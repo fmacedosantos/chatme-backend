6f5b308a3f3577df037ab46ad4496b605e0e8f3f
fmacedosantos <felipemacedo986@gmail.com>
2025-02-21 15:01:29 -0300

    basic configs

---
+++ src/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/models/user.model';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<User | null> {
+    const user = await this.userRepo.findOne({ where: { username } });
+    if (user && await bcrypt.compare(password, user.password)) {
+      return user;
+    }
+    return null;
+  }
+
+  async login(username: string, password: string) {
+    const user = await this.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+    const payload = { username: user.username, sub: user.id };
+    return { access_token: this.jwtService.sign(payload) };
+  }
+}
